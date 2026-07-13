9a735a8b493262488c5f577452ea873f7a9c801f
Vanshaj Verma <vansh@Vanshajs-MacBook-Air-3.local>
2026-07-14 02:33:03 +0530

    feat: Pydantic schemas for Project, Experiment, Dataset, Analysis (Create + Response)

---
+++ schemas.py
@@ -0,0 +1,81 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional, List
+
+# Project Schemas
+class ProjectBase(BaseModel):
+## base schema for project, used for both creation and retrieval
+    name: str
+    desc: Optional[str] = None
+
+class ProjectCreate(ProjectBase):
+    pass
+    ## inherits from ProjectBase
+
+class Project(ProjectBase):
+    id: int
+    created_at: datetime
+    ## inherits from ProjectBase and adds id and created_at fields
+ 
+    class Config:
+        from_attributes=True
+        ### allows Pydantic to read data from ORM models
+        ### to convert SQLAlchemy models to Pydantic models for output responses
+
+# Experiment Schemas
+class ExperimentBase(BaseModel):
+    title: str
+    instrument: Optional[str] = None
+    freq: Optional[float] = None
+    voltage: Optional[float] = None
+    notes: Optional[str] = None
+    ### optional fields if any, can be left empty if not provided
+
+class ExperimentCreate(ExperimentBase):
+    project_id: int
+
+class Experiment(ExperimentBase):
+    id: int
+    project_id: int
+    created_at: datetime
+ 
+    class Config:
+        from_attributes = True
+
+# Dataset Schemas
+class DatasetBase(BaseModel):
+    filename: str
+    filepath: str
+ 
+ 
+class DatasetCreate(DatasetBase):
+    exp_id: int
+ 
+ 
+class Dataset(DatasetBase):
+    id: int
+    exp_id: int
+    uploaded_at: datetime
+ 
+    class Config:
+        from_attributes = True
+
+# Analysis Schemas
+class AnalysisBase(BaseModel):
+    mean_cp: Optional[float] = None
+    std_cp: Optional[float] = None
+    correlation: Optional[float] = None
+    expanded_u: Optional[float] = None
+ 
+class AnalysisCreate(AnalysisBase):
+    dataset_id: int
+ 
+ 
+class Analysis(AnalysisBase):
+    id: int
+    dataset_id: int
+    created_at: datetime
+ 
+    class Config:
+        from_attributes = True
+ 
