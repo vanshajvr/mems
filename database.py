9a68ddac1e41fe9e3796ec9b605969a278239d1c
Vanshaj Verma <vansh@Vanshajs-MacBook-Air-3.local>
2026-07-14 13:47:59 +0530

    feat: updated comments on the file database.py

--- database.py
+++ database.py
@@ -2,20 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL="sqlite:///./mems.db"
+DATABASE_URL="sqlite:///./mems.db" ## address of the database file
 
 engine=create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
-)
+) ## create engine to connect to the database
 
 SessionLocal=sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
-)
+) ## create a sessionmaker class to create database sessions
 
-Base=declarative_base()
+Base=declarative_base() ## create a base class for the models to inherit from 
 
 def get_db():
     db=SessionLocal()
@@ -23,3 +23,4 @@ def get_db():
         yield db
     finally:
         db.close()
+## a dependency function to get a database session and close it after use
